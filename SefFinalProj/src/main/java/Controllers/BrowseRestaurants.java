33a85e3da1c480f4931855d51db85de563e7ad73
cadar1 <82055409+cadar1@users.noreply.github.com>
2021-05-26 03:38:08 +0300

    SEF-6 Adds Browse

--- SefFinalProj/src/main/java/Controllers/BrowseRestaurants.java
+++ SefFinalProj/src/main/java/Controllers/BrowseRestaurants.java
@@ -1,4 +1,21 @@
 package Controllers;
 
+import Start.Main;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 public class BrowseRestaurants {
+    public void cancelOnAction(ActionEvent event) {
+        Main.getStg().close();
+    }
+    public void leaveRatingOnAction(ActionEvent event) throws IOException {
+        Main.getInstance().changeSceneSmall("/CustomerRating.fxml", "Rating");
+    }
+    public void leaveReviewOnAction(ActionEvent event) throws IOException {
+        Main.getInstance().changeSceneSmall("/CustomerReview.fxml", "Review");
+    }
+    public void BrowseRestaurantsOnAction(ActionEvent event) throws IOException {
+        Main.getInstance().changeSceneSmall("/BrowseRestaurantsMenu.fxml", "Browse");
+    }
 }
